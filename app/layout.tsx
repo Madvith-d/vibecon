b6c30b008adca56b0588b4e648fabd2d22f97e75
madvith-d <madroux930@gmail.com>
2025-09-02 21:26:56 +0530

    Add Kinde authentication integration and update layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar1 } from "@/components/ui/navbar-1" ;
 import { ClerkProvider } from "@clerk/nextjs";
+import { AuthProvider } from "@/components/general/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <AuthProvider>
       <html lang="en" className="dark">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -34,6 +35,6 @@ export default function RootLayout({
           {children}
         </body>
       </html>
-    </ClerkProvider>
+    </AuthProvider>
   );
 }
--- components/ui/navbar-1.tsx
+++ components/ui/navbar-1.tsx
@@ -1,30 +1,47 @@
-"use client" 
+ 
 
 import * as React from "react"
-import { useState } from "react"
+// import { useState } from "react"
 import { motion, AnimatePresence } from "motion/react"
-import { Link, Menu, X } from "lucide-react"
+import { Menu, X } from "lucide-react"
+import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs"
+// import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs" 
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import {LogoutLink,LoginLink,RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { div } from "framer-motion/client"
+const Navbar1 = async () => {
+ 
+  const {isAuthenticated} = getKindeServerSession()
+  const isUserAuthenticated = await isAuthenticated();
 
-const Navbar1 = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const { isSignedIn } = useUser()
+  
 
-  const toggleMenu = () => setIsOpen(!isOpen)
 
   return (
-    <div className="p-8 border-b flex flex-row justify-between items-center rounded-full w-full my-6 h-16 relative z-10" >
+    <div className="p-8 border-b flex flex-row justify-between items-center rounded-full w-[80%] mx-auto my-6 h-16 relative z-10 sticky top-3 backdrop-blur-md bg-background" >
      <div className="flex flex-row items-center gap-4">
       <h1 className="text-2xl font-bold color-red-500">Vibe<span className="text-primary font-bold text-red-500 ">Con</span></h1>
-      <Button variant="outline">
+      <Button variant="ghost">
         <Link href="/dashboard">Dashboard</Link>
       </Button>
      </div>
      <div>
-      <Button variant="outline" className="rounded-full">
-        <Link href="/">Home</Link>
-      </Button>
+      {isUserAuthenticated ? (
+        <LogoutLink>
+          <Button variant="ghost">Logout</Button>
+        </LogoutLink>
+      ) : (
+        <div className="flex flex-row items-center gap-4">
+          <LoginLink>
+            <Button variant="ghost">Login</Button>
+          </LoginLink>
+          <RegisterLink>
+            <Button variant="ghost">Register</Button>
+          </RegisterLink>
+        </div>
+        
+      )}
      </div>
     </div>
   )
